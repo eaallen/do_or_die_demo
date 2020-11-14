0fcddc2f29e557b8feed77b5378190ba54288a0a
eaallen <elijah.a.allen@gmail.com>
2020-11-14 15:45:50 -0700

    n

--- src/demos/pieVote/AllPies.js
+++ src/demos/pieVote/AllPies.js
@@ -11,7 +11,7 @@ export default function AllPies() {
             {cards.map(x => {
                 return <div className="row" key={x}>
                     <div className="col-sm-8 mx-auto mb-5">
-                        <FlipCard/>
+                        <Card/>
                     </div>
                 </div>
             })}

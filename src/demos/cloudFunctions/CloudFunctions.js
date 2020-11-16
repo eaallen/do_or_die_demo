88725c8eac5680ba1a97502b012c481b57dc37e2
eaallen <elijah.a.allen@gmail.com>
2020-11-16 07:13:30 -0700

    impossible

--- src/demos/cloudFunctions/CloudFunctions.js
+++ src/demos/cloudFunctions/CloudFunctions.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import axios from 'axios'
 export default function CloudFunctions(){
+    const[data,setData] = React.useState(null)
     const obj ={
-        title:'PIE OR CRY',
+        title:'this will be good!',
         desc:'You\'re going to regret it either way.',
         img:'',
         votes:0,
     }
     const vote ={
-        id:"GSnDCGg9PUdf4A3BsrQc",
+        id:"4dcXx2zIeMU2Q0SNiy1w",
         vote:1
+    }
+    const getData = async() =>{
+        const data = await axios.get("http://localhost:5001/doordiedemo/us-central1/getPie")
+        setData(data.data.result)
     }
     return<div>
         <h1>
@@ -18,7 +23,19 @@ export default function CloudFunctions(){
         <button className='btn btn-dark' onClick={x=>{axios.get("http://localhost:5001/doordiedemo/us-central1/addPie?text="+encodeURIComponent(JSON.stringify(obj)))}}>Test functino</button>
         <br/>
         <button className='btn btn-light' onClick={x=>{axios.get("http://localhost:5001/doordiedemo/us-central1/votePie?text="+encodeURIComponent(JSON.stringify(vote)))}}>
-            Test up vote (GSnDCGg9PUdf4A3BsrQc)
+            Test up vote (4dcXx2zIeMU2Q0SNiy1w)
         </button>
+        <br/>
+        <button className='btn btn-success' onClick={getData}>
+            get data
+        </button>
+        {!data?null:<div>
+            we have data    
+            {data.map(x=>{
+                return Object.values(x).map((item,idx)=>{
+                    return <div key={idx}>{item}</div>
+                })
+            })}
+        </div>}
     </div>
 } 
